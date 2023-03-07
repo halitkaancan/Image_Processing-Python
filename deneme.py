ee15877638f5e3420857d381e67c91c84ff596c3
Halit Kaan Can <halitkaancan@gmail.com>
2023-03-07 19:40:51 +0300

    last commit

--- deneme.py
+++ deneme.py
@@ -2,15 +2,26 @@ import cv2
 import numpy as np
 
 # set intensity threshold
-thresh1 = 140
+thresh1 = 160
 thresh2 = 180
+thresh3 = 200
 
-# read image
-img = cv2.imread('x.jpg')
+thresh4 = 220
+
+
+# define rescale function
+def rescale(frame, scale=0.5):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    boyut = (width, height)
+    return cv2.resize(frame, boyut, interpolation=cv2.INTER_AREA)
+
+# read image and rescale
+img = cv2.imread('xy.jpg')
+img = rescale(img)
 
 # convert img to grayscale intensity
 intensity = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
 
 # get img where intensity is larger than thresh
 data = img[(intensity>thresh1)&(thresh2>intensity)]
@@ -25,7 +36,9 @@ for (y,x),(b,g,r)in zip(coords,data):
 # draw red pixels on intensity image where larger than thresh
 result = intensity.copy()
 result = cv2.cvtColor(result, cv2.COLOR_GRAY2BGR)
-result[intensity>thresh1] = (0,0,255)
+result[(intensity>thresh1)&(thresh2>intensity)] = (0,0,255)
+result[(intensity>thresh2)&(thresh3>intensity)] = (0,255,0)
+result[(intensity>thresh3)&(thresh4>intensity)] = (255,0,0)
 
 # save result
 cv2.imwrite('green_region_190.jpg', result)
